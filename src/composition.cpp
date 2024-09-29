b487ccc9deb63a3582efa5255440466db4f7a55b
Erick <ericksuzart@gmail.com>
2024-09-29 20:37:00 -0300

    feat: update composition

--- src/composition.cpp
+++ src/composition.cpp
@@ -1,10 +1,26 @@
+// Copyright 2024 Oxebots
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
 #include <rclcpp/rclcpp.hpp>
+
 #include "oxebots_comms/game_receiver.hpp"
 
-int main(int argc, char ** argv) {
+int main(int argc, char ** argv)
+{
   rclcpp::init(argc, argv);
-  boost::asio::io_context io_context;
-  auto game_receiver = std::make_shared<GameReceiver>(io_context);
+  auto game_receiver = std::make_shared<GameReceiver>();
   rclcpp::executors::MultiThreadedExecutor executor;
   executor.add_node(game_receiver);
   executor.spin();
