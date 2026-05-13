de89f7fb7f9243493f75cb09f6df010345a4d514
matheuscctb <matheusrafael@ufba.br>
2026-05-13 23:57:42 +0000

    feat: colocando novo simulador e mantendo grsim

---
+++ include/oxebots_comms/ra_controller.hpp
@@ -0,0 +1,47 @@
+// Copyright 2024 Oxebots
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+#pragma once
+
+#include <rclcpp/rclcpp.hpp>
+
+#include <string>
+#include <vector>
+#include <map>
+
+#include "oxebots_comms/udp_sender.hpp"
+#include "oxebots_interfaces/msg/robot_cmd.hpp"
+#include "oxebots_interfaces/msg/game_data.hpp"
+#include "oxebots_interfaces/ssl_simulation_robot_control.pb.h"
+
+class RAController : public rclcpp::Node
+{
+   private:
+    std::unique_ptr<UdpSender<RobotControl>> udp_sender_;
+    rclcpp::Subscription<oxebots_interfaces::msg::RobotCmd>::SharedPtr command_subscription_;
+    rclcpp::Subscription<oxebots_interfaces::msg::GameData>::SharedPtr game_data_subscription_;
+    
+    bool is_yellow_team_;
+    std::map<uint32_t, float> robot_orientations_;
+
+   public:
+    RAController();
+
+    ~RAController();
+
+   private:
+    void command_callback(const oxebots_interfaces::msg::RobotCmd::SharedPtr msg);
+    void game_data_callback(const oxebots_interfaces::msg::GameData::SharedPtr msg);
+};
---
+++ src/ra_controller.cpp
@@ -0,0 +1,118 @@
+// Copyright 2024 Oxebots
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+#include "oxebots_comms/ra_controller.hpp"
+#include <cmath>
+
+RAController::RAController() : rclcpp::Node("ra_controller_node")
+{
+    RCLCPP_INFO(this->get_logger(), "Starting RA controller module (Local Velocity)...");
+
+    // Declare parameters
+    const std::string simulator_ip = declare_parameter<std::string>("simulator_ip", "127.0.0.1");
+    int simulator_port = declare_parameter<int>("simulator_port", 10301);
+    const std::string interface_ip = declare_parameter<std::string>("interface_ip", "");
+    is_yellow_team_ = declare_parameter<bool>("is_yellow_team", false);
+    const std::string command_topic = declare_parameter<std::string>("command_topic", "robot_commands");
+    const std::string game_data_topic = declare_parameter<std::string>("game_data_topic", "game_data");
+
+    // Port logic for grSim/RA: Blue=10301, Yellow=10302
+    if (is_yellow_team_ && simulator_port == 10301) {
+        RCLCPP_INFO(get_logger(), "Time amarelo detectado. Ajustando porta para 10302 automaticamente.");
+        simulator_port = 10302;
+    } else if (!is_yellow_team_ && simulator_port == 10302) {
+        RCLCPP_INFO(get_logger(), "Time azul detectado. Ajustando porta para 10301 automaticamente.");
+        simulator_port = 10301;
+    }
+
+    udp_sender_ = std::make_unique<UdpSender<RobotControl>>(simulator_ip, static_cast<uint16_t>(simulator_port), interface_ip);
+
+    // Create subscription for robot commands
+    command_subscription_ = create_subscription<oxebots_interfaces::msg::RobotCmd>(
+      command_topic, 10, std::bind(&RAController::command_callback, this, std::placeholders::_1));
+
+    // Create subscription for game data (to get orientation)
+    game_data_subscription_ = create_subscription<oxebots_interfaces::msg::GameData>(
+      game_data_topic, 10, std::bind(&RAController::game_data_callback, this, std::placeholders::_1));
+
+    RCLCPP_INFO(this->get_logger(), "RA controller module started, sending to %s:%d as %s team",
+                simulator_ip.c_str(), simulator_port, is_yellow_team_ ? "yellow" : "blue");
+}
+
+RAController::~RAController() { RCLCPP_INFO(this->get_logger(), "Stopping RA controller module..."); }
+
+void RAController::game_data_callback(const oxebots_interfaces::msg::GameData::SharedPtr msg)
+{
+    // Update orientations for our team (allies)
+    for (const auto & robot : msg->robots.allies)
+    {
+        robot_orientations_[robot.id] = robot.orientation;
+    }
+}
+
+void RAController::command_callback(const oxebots_interfaces::msg::RobotCmd::SharedPtr msg)
+{
+    RobotControl packet;
+
+    for (const auto & robot_command_data : msg->robots)
+    {
+        auto * robot_cmd = packet.add_robot_commands();
+
+        robot_cmd->set_id(robot_command_data.id);
+        robot_cmd->set_kick_speed(robot_command_data.kick_speed);
+        robot_cmd->set_dribbler_speed(0);
+        robot_cmd->set_kick_angle(0);
+
+        auto * move_cmd = robot_cmd->mutable_move_command();
+        auto * local_velocity = move_cmd->mutable_local_velocity();
+
+        // Get current orientation (default to 0 if not found)
+        float theta = 0.0f;
+        if (robot_orientations_.find(robot_command_data.id) != robot_orientations_.end()) {
+            theta = robot_orientations_[robot_command_data.id];
+        }
+
+        // Coordinate transformation: Global to Local
+        // v_forward = vx * cos(theta) + vy * sin(theta)
+        // v_left = -vx * sin(theta) + vy * cos(theta)
+        float vx = robot_command_data.x_velocity;
+        float vy = robot_command_data.y_velocity;
+        
+        float v_forward = vx * std::cos(theta) + vy * std::sin(theta);
+        float v_left = -vx * std::sin(theta) + vy * std::cos(theta);
+
+        local_velocity->set_forward(v_forward);
+        local_velocity->set_left(v_left);
+        local_velocity->set_angular(robot_command_data.angular_velocity);
+
+        // Log para debug (aparecerá a cada 1 segundo para não poluir)
+        RCLCPP_INFO_THROTTLE(this->get_logger(), *this->get_clock(), 1000, 
+                             "Robot %d: Global(%.2f, %.2f) -> Local(%.2f, %.2f) at theta=%.2f", 
+                             robot_command_data.id, vx, vy, v_forward, v_left, theta);
+    }
+
+    if (packet.robot_commands_size() > 0)
+    {
+        udp_sender_->send(packet);
+    }
+}
+
+int main(int argc, char * argv[])
+{
+    rclcpp::init(argc, argv);
+    rclcpp::spin(std::make_shared<RAController>());
+    rclcpp::shutdown();
+    return 0;
+}
